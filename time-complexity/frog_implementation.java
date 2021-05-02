ffd6416bbbc96776007e32456dd948688a077860
Christian Pari <christianpari@outlook.com>
2021-05-02 18:52:07 -0400

    Completed a time complexity challenge to find minimum number of jumps needed to reach a value

---
+++ time-complexity/frog_implementation.java
@@ -0,0 +1,22 @@
+public class frog_implementation {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 85;
+    int d = 30;
+    // below passed w/ O(1)
+    y -= x;
+    int jumps = y / d + (y % d != 0 ? 1 : 0);
+    System.out.println(jumps);
+    // TOO LONG
+//    int jumps = 0;
+//    while (true) {
+//      if (x < y) {
+//        x += d;
+//        jumps++;
+//      } else {
+//        System.out.println(jumps);
+//        break;
+//      }
+    }
+  }
+}
